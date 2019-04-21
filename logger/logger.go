2ac0ba6e783e4a8271b40682b1ea13ad2655008e
Steven Ayers <sayers@equalexperts.com>
2019-04-21 10:08:44 +0100

    Removing testing function from logger

--- logger/logger.go
+++ logger/logger.go
@@ -37,19 +37,3 @@ func Logger(handler http.Handler, name string) http.Handler {
 
 	})
 }
-
-func Logger2(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		inner.ServeHTTP(w, r)
-
-		log.Printf(
-			"%s\t%s\t%s\t%s",
-			r.Method,
-			r.RequestURI,
-			name,
-			time.Since(start),
-		)
-	})
-}
